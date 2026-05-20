71f9b1ccf892baa876933470ea92a413c6691fb9
Joe Edwards <webdev@onpoyntcreative.com>
2026-05-20 13:13:07 +0100

    hourly

--- includes/class-prn-cron.php
+++ includes/class-prn-cron.php
@@ -9,16 +9,19 @@ class PRN_Cron
     /**
      * Plugin activation
      */
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class PRN_Cron
+{
+    /**
+     * Plugin activation
+     */
     public static function activate()
     {
-        /**
-         * Add custom 15-minute schedule
-         */
-        add_filter(
-            'cron_schedules',
-            [__CLASS__, 'add_cron_intervals']
-        );
-
         /**
          * Schedule cron if not already scheduled
          */
@@ -26,13 +29,13 @@ class PRN_Cron
 
             wp_schedule_event(
                 time(),
-                'prn_every_fifteen_minutes',
+                'hourly',
                 'prn_import_cron'
             );
         }
 
         PRN_Logger::log(
-            'Cron activated'
+            'Cron activated (hourly)'
         );
     }
 
@@ -40,14 +43,13 @@ class PRN_Cron
      * Plugin deactivation
      */
     public static function deactivate()
-    {
-        wp_clear_scheduled_hook(
-            'prn_import_cron'
-        );
+        {
+            wp_clear_scheduled_hook('prn_import_cron');
 
-        PRN_Logger::log(
-            'Cron deactivated'
-        );
+            PRN_Logger::log(
+                'Cron deactivated'
+            );
+        }
     }
 
     /**
